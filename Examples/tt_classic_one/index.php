427a535dd1e7d9a7ba5559ef1b6e9d65afd664fa
Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
2014-04-26 15:37:46 +0200

    changed description

--- Examples/tt_classic_one/index.php
+++ Examples/tt_classic_one/index.php
@@ -18,8 +18,9 @@
     // initialize the TemplateTools
     require_once WB_PATH.'/kit2/extension/phpmanufaktur/phpManufaktur/TemplateTools/initialize.php';  
 
-    // include a simple <head> .. </head> section
-    $template['twig']->display('twig/head.twig');
+    // use the unchanged pattern to include a simple <head> section
+    // the pattern will also load the /css/screen.css !
+    $template['twig']->display('@pattern/classic/head.simple.twig');
   ?>
   <body>
     <div class="body-container">
