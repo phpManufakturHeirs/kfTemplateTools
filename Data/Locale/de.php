fb981f8d71d949989c9ce16cfd5ab17292207a04
Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
2014-05-02 15:28:49 +0200

    just in porgress

--- Data/Locale/de.php
+++ Data/Locale/de.php
@@ -47,11 +47,16 @@ return array(
     'Previous'
         => 'Vorherige',
 
+    '<em>Ridiculously Responsive Social Sharing Buttons</em>, using about <strong>%percent%%</strong> width of the <em>Panel Vontainer</em>.'
+        => '<em>Ridiculously Responsive Social Sharing Buttons</em>, verwenden etwa <strong>%percent%%</strong> der zur Verfügung stehenden Breite des <em>Panel Container</em>.',
+
     'Search'
         => 'Suche',
 
     'The Droplet %droplet% does not exists!'
         => 'Das Droplet <i>%droplet%</i> existiert nicht!',
+    'The <em>%pattern_type% Pager</em> enable you to step forward and backward through the whole Website.'
+        => 'Der <em>%pattern_type% Pager</em> ermöglicht es Ihnen sich vorwärts und rückwärts durch die gesamte Website zu bewegen.',
 
     'User account'
         => 'Benutzerkonto',
@@ -60,4 +65,7 @@ return array(
 
     'Welcome back, %name%'
         => 'Herzlich willkommen, %name%!',
+    'Welcome to the start page of %CMS_TITLE%!'
+        => 'Herzlich willkommen auf der Startseite von %CMS_TITLE%!',
+    
 );
