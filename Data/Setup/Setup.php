ce610af0366ad3a1bdc5f69556d962ff4465547c
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-05-04 06:25:08 +0200

    The NEWS addon has a small bug (missing a global declaration for $MOD_NEWS) which will be now fixed by the Setup/Update

--- Data/Setup/Setup.php
+++ Data/Setup/Setup.php
@@ -17,10 +17,10 @@ use Symfony\Component\Finder\Finder;
 class Setup
 {
     protected $app = null;
-    
+
     /**
      * Constructor
-     * 
+     *
      * @param Application $app
      */
     public function __construct(Application $app = null)
@@ -29,10 +29,10 @@ class Setup
             $this->app = $app;
         }
     }
-    
+
     /**
      * Register the given Template in the CMS
-     * 
+     *
      * @param string $template_directory_name
      * @return boolean
      */
@@ -40,11 +40,11 @@ class Setup
     {
         $info_path = CMS_PATH.'/templates/'.$template_directory_name.'/info.php';
         if (!$this->app['filesystem']->exists($info_path)) {
-            $this->app['monolog']->addError('File does not exists: '.$info_path, 
+            $this->app['monolog']->addError('File does not exists: '.$info_path,
                 array(__METHOD__, __LINE__));
             return false;
         }
-        
+
         // initialize the info.php variables
         $template_directory = null;
         $template_name = null;
@@ -55,7 +55,7 @@ class Setup
         $template_author = null;
         $template_license = null;
         $template_guid = null;
-        
+
         // require the info.php
         require $info_path;
 
@@ -63,12 +63,12 @@ class Setup
         $SQL = "SELECT `addon_id` FROM `".CMS_TABLE_PREFIX."addons` WHERE `directory`='$template_directory_name' ".
             "AND `type`='template'";
         $addon_id = $this->app['db']->fetchColumn($SQL);
-        
+
         if ($addon_id > 0) {
             // the template is already registered, remove it.
             $this->app['db']->delete(CMS_TABLE_PREFIX.'addons', array('addon_id' => $addon_id));
         }
-        
+
         $data = array(
             'directory' => $template_directory,
             'name' => $template_name,
@@ -80,19 +80,19 @@ class Setup
             'author' => addslashes($template_author),
             'license' => addslashes($template_license)
         );
-        
+
         if (CMS_TYPE != 'WebsiteBaker') {
             $data['guid'] = $template_guid;
         }
-        
+
         $this->app['db']->insert(CMS_TABLE_PREFIX.'addons', $data);
-        
+
         return true;
     }
-    
+
     /**
      * Install all example templates within the CMS and register them
-     * 
+     *
      */
     public function install_templates()
     {
@@ -102,22 +102,22 @@ class Setup
         $finder->depth('== 0');
         // get all directories in the /Examples
         $finder->directories()->in(MANUFAKTUR_PATH.'/TemplateTools/Examples');
-        
+
         foreach ($finder as $directory) {
             $template_name = $directory->getFilename();
             $target_directory = CMS_PATH.'/templates/'.$template_name;
-        
+
             if ($this->app['filesystem']->exists($target_directory)) {
                 // the template already exists - remove it
                 $this->app['filesystem']->remove($target_directory);
             }
             // create the template directory
             $this->app['filesystem']->mkdir($target_directory);
-        
+
             // get all files and directories from source
             $source = new Finder();
             $source->in($directory->getRealpath());
-        
+
             foreach ($source as $item) {
                 if ($item->isDir()) {
                     // create the directory in the target
@@ -128,26 +128,51 @@ class Setup
                     $this->app['filesystem']->copy($item->getRealPath(), $target_directory.'/'.$item->getRelativePathname());
                 }
             }
-        
+
             // ok - all files are copied, now update the CMS database
             if ($this->register_template($template_name)) {
                 $this->app['monolog']->addDebug('Successfull installed and registered the template '.$template_name);
             }
         }
     }
-    
+
+    /**
+     * Process different CMS Bugfixes
+     *
+     */
+    public function cms_bugfix()
+    {
+        if ($this->app['filesystem']->exists(CMS_PATH.'/modules/news/view.php')) {
+            $content = file_get_contents(CMS_PATH.'/modules/news/view.php');
+            if (false === (strpos($content, 'global $MOD_NEWS;'))) {
+                // proceed bugfix, set the global variable $MOD_NEWS
+                $this->app['filesystem']->remove(CMS_PATH.'/modules/news/view.php.bak');
+                file_put_contents(CMS_PATH.'/modules/news/view.php.bak', $content);
+
+                $new_content = substr($content, stripos($content, '<?php')+strlen('<?php'));
+                $insert = '<?php'."\n\n".'// Bugfix: missing the global variable $MOD_NEWS (proceeded by the TemplateTools)'.
+                    "\n".'global $MOD_NEWS;'."\n";
+                file_put_contents(CMS_PATH.'/modules/news/view.php', $insert.$new_content);
+                $this->app['monolog']->addDebug('Insert Bugfix for the CMS NEWS Addon.');
+            }
+        }
+    }
+
     /**
      * Execute the setup routine for the TemplateTools
-     * 
+     *
      * @param Application $app
      */
     public function exec(Application $app)
     {
         $this->app = $app;
-        
+
+        // CMS Bugfixes
+        $this->cms_bugfix();
+
         // install the templates
         $this->install_templates();
-        
+
         return $app['translator']->trans('Successfull installed the extension %extension%.',
                 array('%extension%' => 'TemplateTools'));
     }
--- Data/Setup/Update.php
+++ Data/Setup/Update.php
@@ -16,19 +16,25 @@ use Silex\Application;
 class Update
 {
     protected $app = null;
-    
+
     /**
      * Execute the Update for the TemplateTools
-     * 
+     *
      * @param Application $app
      */
     public function exec(Application $app)
     {
         $this->app = $app;
-        
+
+        // initialize the SETUP
         $Setup = new Setup($app);
+
+        // proceed CMS bugfixes
+        $Setup->cms_bugfix();
+
+        // install the templates
         $Setup->install_templates();
-        
+
         return $app['translator']->trans('Successfull updated the extension %extension%.',
             array('%extension%' => 'TemplateTools'));
     }
