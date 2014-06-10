32b2f8e44fc581eacc997edcfdbdbd08fedf194f
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-06-11 01:31:02 +0200

    changed route to access commands directly

--- Control/kitCommandFunctions.php
+++ Control/kitCommandFunctions.php
@@ -39,9 +39,12 @@ class kitCommandFunctions
     {
         $params = array(
             'cms' => array(
+                'type' => CMS_TYPE,
+                'version' => CMS_VERSION,
                 'locale' => PAGE_LOCALE,
                 'page_id' => PAGE_ID,
                 'page_url' => PAGE_URL,
+                'page_visibility' => PAGE_VISIBILITY,
                 'user' => array(
                     'id' => CMS_USER_ID,
                     'name' => CMS_USER_USERNAME,
@@ -54,7 +57,7 @@ class kitCommandFunctions
         );
 
         $option = array(
-            CURLOPT_URL => FRAMEWORK_URL.'/kit_command/'.strtolower($command),
+            CURLOPT_URL => FRAMEWORK_URL.'/command/'.strtolower($command),
             CURLOPT_HEADER => false,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_RETURNTRANSFER => true,
